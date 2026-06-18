d026a671aff0b921330f2fa43efe0bec9f115764
Winston White <whitew2015@fau.edu>
2026-06-18 14:15:53 -0400

    Update component to showcase user's name, picture, followers and repos

--- github-profile-analyzer/src/components/UserProfile.jsx
+++ github-profile-analyzer/src/components/UserProfile.jsx
@@ -1,3 +1,18 @@
 export default function UserProfile({ user }) {
-    if (!user) return null;
+    if (!user) return null
+
+    return (
+        <div>
+            <img 
+                src={user.avatar_url}
+                alt={user.login}
+                width="150"
+            />
+
+            <h2>{user.name}</h2>
+            <p>{user.bio}</p>
+            <p>Followers: {user.followers}</p>
+            <p>Repositories: {user.public_repos}</p>
+        </div>
+    )
 }
