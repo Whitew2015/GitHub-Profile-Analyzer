96456e6d245b2bc986fb5979184cdbf8d8e99fcf
Winston White <whitew2015@fau.edu>
2026-06-19 16:53:02 -0400

    Add error message and loading state

--- github-profile-analyzer/src/App.jsx
+++ github-profile-analyzer/src/App.jsx
@@ -7,13 +7,29 @@ import { getUser } from './services/githubApi';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async(username) => {
+
+    if (!username.trim()) {
+      setUser(null);
+      setError("Please enter a Github username");
+      return;
+    }
+
     try {
+      setLoading(true);
+      setError("");
+
       const data = await getUser(username);
       setUser(data);
+    // eslint-disable-next-line no-unused-vars
     } catch (error) {
-      console.error(error);
+      setUser(null);
+      setError("User not found");
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -22,7 +38,11 @@ function App() {
       <h1>GitHub Profile Analyzer</h1>
       <SearchBar onSearch={handleSearch} />
 
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
       <UserProfile user={user} />
+      
     </div>
   );
 }
