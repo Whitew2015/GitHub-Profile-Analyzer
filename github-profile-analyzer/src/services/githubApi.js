0ca237460392c8d030311e0d16ed6b71e571a61e
Winston White <whitew2015@fau.edu>
2026-06-19 17:02:54 -0400

    Add function for calling user's repos

--- github-profile-analyzer/src/services/githubApi.js
+++ github-profile-analyzer/src/services/githubApi.js
@@ -9,3 +9,15 @@ export async function getUser(username) {
 
     return response.json();
 }
+
+export async function getRepos(username) {
+    const response = await fetch(
+        `https://api.github.com/users/${username}/repos`
+    );
+
+    if (!response.ok) {
+        throw new Error("Repositories not found");
+    }
+
+    return response.json();
+}
