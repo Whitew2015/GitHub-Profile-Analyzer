2e30ba085ead79df310171afb5b7817a0684f6d6
Winston White <whitew2015@fau.edu>
2026-07-09 18:46:58 -0400

    add RepoList component to display repo info

--- github-profile-analyzer/src/App.jsx
+++ github-profile-analyzer/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import SearchBar from './components/SearchBar';
 import UserProfile from './components/UserProfile';
-import { getUser } from './services/githubApi';
+import { getRepos, getUser } from './services/githubApi';
+import RepoList from './components/RepoList';
 
 
 function App() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
 
   const handleSearch = async(username) => {
 
@@ -24,6 +26,9 @@ function App() {
 
       const data = await getUser(username);
       setUser(data);
+
+      const repoData = await getRepos(username);
+      setRepos(repoData);
     // eslint-disable-next-line no-unused-vars
     } catch (error) {
       setUser(null);
@@ -42,6 +47,7 @@ function App() {
       {error && <p>{error}</p>}
 
       <UserProfile user={user} />
+      <RepoList repos={repos} />
       
     </div>
   );
