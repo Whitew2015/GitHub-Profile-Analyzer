eef928063b442fcbb5ba65e361a3c1eb1c66ccf0
Winston White <whitew2015@fau.edu>
2026-07-13 17:17:14 -0400

    test rendering of repos

--- github-profile-analyzer/src/components/RepoList.jsx
+++ github-profile-analyzer/src/components/RepoList.jsx
@@ -18,7 +18,7 @@ export default function RepoList({ repos }) {
                         </a>
                     </h3>
                     <p>{repo.description}</p>
-                    <p>Language: {repo.language ?? "Unknown"}</p>
+                    <p>Language: {repo.language ?? "Not Specified"}</p>
                     <p>⭐ Stars: {repo.stargazers_count}</p>
                 </div>
             ))}
