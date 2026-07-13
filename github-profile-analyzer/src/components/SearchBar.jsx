abc16e3be0c0cf607be4b08d07dab79600b2d331
Winston White <whitew2015@fau.edu>
2026-07-13 13:03:16 -0400

    update input tag

--- github-profile-analyzer/src/components/SearchBar.jsx
+++ github-profile-analyzer/src/components/SearchBar.jsx
@@ -11,8 +11,15 @@ export default function SearchBar({ onSearch }) {
                 placeholder="Github username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        onSearch(username);
+                    }
+                }}
                 />
-                <button onClick={() => onSearch(username)}>Search</button>
+                <button onClick={() => onSearch(username)}>
+                    Search
+                </button>
 
                 {/*Testing for username appearing on-screen*/}
                 <p>Searching for: {username}</p>
